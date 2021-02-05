4170d42c5b9575df8642585c17c2be2054d7d4b1
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-02-05 17:05:15 -0500

    commit tree 4e2bb0e88d64920b224ffbd942df44fcfa07215f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+if array.length < 2
+    array.join
+elsif array.length > 2
+last = array[-1]
+last = "and #{last}"
+array.pop
+array << last
+answer = array.join(", ")
+answer
+else
+    last = array[-1]
+    last = "and #{last}"
+array.pop
+array << last
+answer = array.join(" ")
+answer
+end
 end
